5bd5249878cf8c38784eaf0a31de334077bffa98
swcool <ac2k888@yahoo.com>
2011-06-26 12:27:37 -0700

    update for heroku site login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,12 +8,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     require "openid/fetchers"
     OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
      
-    # providers with id/secret, you need to sign up for their services (see below) and enter the parameters here
-    provider :facebook, 'APP_ID', 'APP_SECRET'
-    provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-    provider :github, 'CLIENT ID', 'SECRET'
-    provider :tsina, 'CLIENT ID', 'SECRET'
-    
+    # providers with id/secret, you need to sign up for their services (see below) and enter the parameters here  
+    provider :facebook, '171174192945589', '7adf9c708b4f80aee1de6a9211c68e68'
+    provider :twitter, 'UUVVbEaBjm0GrRkG8Mesg', 'Qxi86s8op34KQDokQMOWvjYVJmvP72JDLXur91iiI'
+    provider :github, '82e688018a15aff0c002', '38cfe0c37a1fbd4c30c02726fcd57fa0dab68c5f'
+    provider :tsina, '92783654', 'b8e8af30acaec274d67b119030d9e881'
+
     # generic openid
     provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'openid'
         
@@ -26,8 +26,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :openid, OpenID::Store::Filesystem.new('/tmp'), :name => 'aol', :identifier => 'openid.aol.com'
     provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'myopenid', :identifier => 'myopenid.com'
         
-    # Sign-up urls for Facebook, Twitter, and Github
+    # Sign-up urls for Facebook, Twitter, Github, and Weibo
     # https://developers.facebook.com/setup
     # https://github.com/account/applications/new
     # https://developer.twitter.com/apps/new
+    # http://open.weibo.com/apps/new
   end
